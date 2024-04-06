30bbe4c57850f22d004b220e1e722dd2d5f5107e
Xabibullayev Kamronbek <simplekux8082@icloud.com>
2024-04-07 00:28:27 +0500

    added

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter a first number");
+string firstInput = Console.ReadLine();
+Console.WriteLine("Enter a first number");
+string secondInput = Console.ReadLine();
+
+int firstNum = Convert.ToInt32(firstInput);
+int secondNum = Convert.ToInt32(secondInput);
+
+Console.WriteLine("Last result");
+Console.WriteLine(firstNum + secondNum); 
